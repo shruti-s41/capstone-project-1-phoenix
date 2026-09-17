bf2a3d88f15c6fcc4ffce26925b60a0067ddb817
Mitesh Sawant <MITESH.SAWANT@HDFCLIFE.COM>
2026-09-17 13:37:26 +0530

    Inital SpringBoot Project Structure

---
+++ src/main/java/org/hdfc/CapstoneProject1PhoenixApplication.java
@@ -0,0 +1,13 @@
+package org.hdfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProject1PhoenixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneProject1PhoenixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hdfc/CapstoneProject1PhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hdfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProject1PhoenixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
